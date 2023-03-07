74bdafece8c790026b0ac070dfc7b411a0e2135a
alexandre artus <artusalexandre@yahoo.fr>
2023-03-07 11:50:23 +0100

    install gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,8 @@ html_doc = Nokogiri::HTML.parse(html_file)
 html_doc.search(".standard-card-new__article-title").each do |element|
   puts element.text.strip
   puts element.attribute("href").value
+  name = element.text.strip
+  link = element.attribute("href").value
+
+  Surfboard.create(name: name, link: link)
 end
