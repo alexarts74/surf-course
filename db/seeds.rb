826b8326b64e466c69573eb1bc151ffd9559eb41
alexandre artus <artusalexandre@yahoo.fr>
2023-03-07 11:39:30 +0100

    install gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+require "nokogiri"
+
+ingredient = "chocolate"
+url = "https://www.bbcgoodfood.com/search/recipes?q=#{ingredient}"
+
+html_file = URI.open(url).read
+html_doc = Nokogiri::HTML.parse(html_file)
+
+html_doc.search(".standard-card-new__article-title").each do |element|
+  puts element.text.strip
+  puts element.attribute("href").value
+end
