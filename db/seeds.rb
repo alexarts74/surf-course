14d5af48ab0d6d25c087dbdb7f826e6076df1e1d
alexandre artus <artusalexandre@yahoo.fr>
2023-03-07 13:49:41 +0100

    Test scraping

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,18 @@
 require "open-uri"
 require "nokogiri"
 
-ingredient = "chocolate"
-url = "https://www.bbcgoodfood.com/search/recipes?q=#{ingredient}"
+
+board = ""
+url = "https://www.boardshop.co.uk/#{board}"
 
 html_file = URI.open(url).read
 html_doc = Nokogiri::HTML.parse(html_file)
 
-html_doc.search(".standard-card-new__article-title").each do |element|
-  puts element.text.strip
-  puts element.attribute("href").value
+html_doc.search(".product-features/ul/li/span").each do |element|
   name = element.text.strip
-  link = element.attribute("href").value
+  level = element.text.strip
+  volume = element.text.strip
+  dimensions = element.text.strip
 
-  Surfboard.create(name: name, link: link)
+  Surfboard.create(name: name, level: level, volume: volume, dimensions: dimensions)
 end
