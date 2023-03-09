9fa652c7bf0f95a4da968ec86a8c7be5e1ba93bb
Rebecca Sinai <rebeccahsinai@gmail.com>
2023-03-09 18:11:43 +0100

    home page updates and test pages for form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  get "test", to: "pages#test"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :surfboards
   # Defines the root path route ("/")
   # root "articles#index"
 end
