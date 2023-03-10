88838a475552569a76a78459204318ee27e70b3e
Rebecca Sinai <rebeccahsinai@gmail.com>
2023-03-10 14:46:09 +0100

    form-keys-colors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   get "test", to: "pages#test"
+  get "testresults", to: "pages#testresults"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :surfboards
   # Defines the root path route ("/")
