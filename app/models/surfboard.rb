d592b6b958eb96bed49b85a8ef86b7d4997258c5
victor lombard <victor@lombard.fr>
2023-03-13 10:38:10 +0100

    validates

--- app/models/surfboard.rb
+++ app/models/surfboard.rb
@@ -1,2 +1,9 @@
 class Surfboard < ApplicationRecord
+  validates :name, presence: true
+  validates :gender, presence: true
+  validates :weight, presence: true
+  validates :size, presence: true
+  validates :objective, presence: true
+  validates :waves, presence: true
+  validates :temperature, presence: true
 end
