6542aa6d65a1ddd3f8c87e744a0b811d253f3104
Victor Lombard <victor@lombard.fr>
2023-03-16 08:19:15 +0100

    form done for real

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -2,12 +2,36 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="slider"
 export default class extends Controller {
-  static targets = ["value"]
+  static targets = ["containerOutput", "output", "input"]
   connect() {
-    console.log("slideeee");
-  }
 
-  update(event) {
-    this.valueTarget.innerHTML = event.currentTarget.value
+    console.log('je suis dans le connect')
+    console.log(this.containerOutputTarget)
+    console.log(this.outputTarget)
+    console.log(this.inputTarget)
+    this.update()
+  }
+// couplé uniquement avec static targets value = uniquement la valeur affiché
+//   update(event) {
+//     this.valueTarget.innerHTML = event.currentTarget.value
+//   }
+// }
+
+ update () {
+     const value = parseInt(this.inputTarget.value, 10)
+    this.outputTarget.innerText = value
+    console.log('je suisd dans update ----------------------------')
+    const minValue = parseInt(this.inputTarget.min, 10)
+    console.log('minValue:', minValue)
+    const maxValue = parseInt(this.inputTarget.max, 10)
+    console.log('maxValue:', maxValue)
+    console.log('value:', value)
+    const maxMargin = this.containerOutputTarget.offsetWidth - this.outputTarget.offsetWidth
+    console.log('maxMargin:', maxMargin)
+    const percent = (value - minValue) * 100 / (maxValue - minValue)
+    console.log('percent:', percent)
+    const margin = maxMargin * percent / 100
+    console.log('margin:', margin)
+    this.outputTarget.style.marginLeft = `${margin}px`
   }
 }
