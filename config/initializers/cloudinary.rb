7b1882b97faf6bf5323af77d831a232991b65e82
alexandre artus <artusalexandre@yahoo.fr>
2023-11-20 10:07:58 +0100

    try fix cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'diwwtbmrv'
+  config.api_key = '556453121524929'
+  config.api_secret = 'gYxdrGIDS17KeUPqcAZJiMi75PY'
+  config.secure = true
+end
