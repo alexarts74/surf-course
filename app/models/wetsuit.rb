ec9069c8de4371a70828aa125a06f717f0b885f6
alexandre artus <artusalexandre@yahoo.fr>
2023-11-20 16:24:01 +0100

    quasi fini

--- app/models/wetsuit.rb
+++ app/models/wetsuit.rb
@@ -1,9 +1,9 @@
 class Wetsuit < ApplicationRecord
   has_one_attached :image
-  # validates :gender, presence: true
-  # validates :weight, presence: true
-  # validates :size, presence: true
-  # validates :objective, presence: true
-  # validates :waves, presence: true
-  # validates :temperature, presence: true
+  validates :gender, presence: true
+  validates :weight, presence: true
+  validates :size, presence: true
+  validates :objective, presence: true
+  validates :waves, presence: true
+  validates :temperature, presence: true
 end
